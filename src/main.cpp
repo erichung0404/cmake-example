a63ad3059182298df0afe3133ba788c4c4ed40b7
Eric Hung <ehung.hung0404@gmail.com>
2023-09-09 16:25:02 +0800

    init project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+namespace {
+using namespace std;
+
+void welcome() {
+    cout << "Hello World!" << endl;
+}
+}  // anonymous namespace
+
+int main() {
+    welcome();
+    return 0;
+}
