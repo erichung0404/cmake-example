23cbb3fc6464273d140404c7c4ed80717a70fb05
Eric Hung <ehung.hung0404@gmail.com>
2023-09-29 17:21:44 +0800

    add day29 codes

---
+++ toolchain/x86_64-w64-mingw32.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_C_COMPILER /root/x-tools/x86_64-w64-mingw32/bin/x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER /root/x-tools/x86_64-w64-mingw32/bin/x86_64-w64-mingw32-g++)
+
+set(CMAKE_FIND_ROOT_PATH /root/x-tools/x86_64-w64-mingw32)
+
+set(CMAKE_SYSROOT /root/x-tools/x86_64-w64-mingw32/x86_64-w64-mingw32/sysroot)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
